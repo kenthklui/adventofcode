a299ca0a48462fdbf038b3e8d1d710a20c371f48
Kent Lui <kenthklui@gmail.com>
2022-12-15 03:37:01 -0500

    Optimize 2022-15-T1 using T2 solution

--- 2022/day15/task1.go
+++ 2022/day15/task1.go
@@ -14,6 +14,13 @@ func intMinMax(a, b int) (int, int) {
 	}
 }
 
+func intAbs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
 type sensor struct {
 	x, y, bx, by int
 }
@@ -24,27 +31,107 @@ func (s sensor) manhattan() int {
 	return x2 - x1 + y2 - y1
 }
 
-func countImpossible(sensors []sensor, row int) int {
-	bm := make(map[int]bool)
-	for _, s := range sensors {
-		y1, y2 := intMinMax(s.y, row)
-		extend := s.manhattan() - (y2 - y1)
-		if extend >= 0 {
-			for x := s.x - extend; x <= s.x+extend; x++ {
-				bm[x] = false
+type beaconRange struct {
+	start, end int
+}
+
+func (br1 beaconRange) greaterThan(br2 beaconRange) bool { return br1.start > br2.end+1 }
+func (br1 beaconRange) lesserThan(br2 beaconRange) bool  { return br1.end+1 < br2.start }
+func (br1 beaconRange) overlap(br2 beaconRange) bool {
+	return br1.start <= br2.end+1 && br2.start <= br1.end+1
+}
+
+type beacons struct {
+	ranges []beaconRange
+}
+
+func NewBeacons() *beacons {
+	return &beacons{make([]beaconRange, 0)}
+}
+
+func (b *beacons) addRange(newRange beaconRange) {
+	n := len(b.ranges)
+	if n == 0 {
+		b.ranges = append(b.ranges, newRange)
+		return
+	}
+
+	if newRange.greaterThan(b.ranges[n-1]) { // larger than everything
+		b.ranges = append(b.ranges, newRange)
+		return
+	} else if newRange.lesserThan(b.ranges[0]) { // smaller than everything
+		b.ranges = append([]beaconRange{newRange}, b.ranges...)
+		return
+	}
+
+	var overlapStartIndex, overlapEndIndex int
+	for i, brs := range b.ranges {
+		if newRange.overlap(brs) {
+			overlapStartIndex, overlapEndIndex = i, i
+			for _, bre := range b.ranges[i+1:] {
+				if newRange.overlap(bre) {
+					overlapEndIndex++
+				} else {
+					break
+				}
 			}
+			break
 		}
 	}
 
+	if firstStart := b.ranges[overlapStartIndex].start; newRange.start > firstStart {
+		newRange.start = firstStart
+	}
+	if lastEnd := b.ranges[overlapEndIndex].end; newRange.end < lastEnd {
+		newRange.end = lastEnd
+	}
+
+	newRanges := append(b.ranges[:overlapStartIndex], newRange)
+	newRanges = append(newRanges, b.ranges[overlapEndIndex+1:]...)
+	b.ranges = newRanges
+}
+
+func buildMap(sensors []sensor, minVal, maxVal int) []*beacons {
+	bm := make([]*beacons, maxVal-minVal+1)
+	for i := range bm {
+		bm[i] = NewBeacons()
+	}
+
+	for _, s := range sensors {
+		man := s.manhattan()
+		for dy := -man; dy <= man; dy++ {
+			y := s.y + dy
+			if y < minVal {
+				continue
+			} else if y > maxVal {
+				break
+			}
+
+			dx := man - intAbs(dy)
+			bm[y-minVal].addRange(beaconRange{s.x - dx, s.x + dx})
+		}
+	}
+
+	return bm
+}
+
+func countOccupied(sensors []sensor, row int) int {
+	occupied := 0
+	bm := buildMap(sensors, row, row)
+	for _, br := range bm[0].ranges {
+		occupied += br.end - br.start + 1
+	}
+
+	// Subtract tiles occupied by beacons
+	beaconX := make(map[int]byte)
 	for _, s := range sensors {
 		if s.by == row {
-			if _, ok := bm[s.bx]; ok {
-				delete(bm, s.bx)
-			}
+			beaconX[s.bx] = 0
 		}
 	}
+	occupied -= len(beaconX)
 
-	return len(bm)
+	return occupied
 }
 
 func readInput() []string {
@@ -81,5 +168,7 @@ func parseInput(input []string) []sensor {
 func main() {
 	input := readInput()
 	sensors := parseInput(input)
-	fmt.Println(countImpossible(sensors, 2000000))
+	occupied := countOccupied(sensors, 2000000)
+
+	fmt.Println(occupied)
 }
