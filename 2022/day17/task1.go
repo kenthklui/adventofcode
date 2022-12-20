de95e3ff9d546a1961cce8438265e0a0199a7867
Kent Lui <kenthklui@gmail.com>
2022-12-20 04:17:55 -0500

    2022-17-T1: refactor for consistency with T2

--- 2022/day17/task1.go
+++ 2022/day17/task1.go
@@ -7,32 +7,36 @@ import (
 	"strings"
 )
 
+const rockCycle = 5
+const caveWidth = 7
+
+type pnt struct {
+	x, y int
+}
+
+func pkgPnts(coords ...int) []pnt {
+	pnts := make([]pnt, 0, len(coords)/2)
+	for i := 0; i < len(coords); i += 2 {
+		pnts = append(pnts, pnt{coords[i], coords[i+1]})
+	}
+	return pnts
+}
+
 type cave struct {
-	rockCount, jetCount, height int
+	rockCount, jetCycle, height int
 	currRock                    *rock
-	space                       [][7]*rock
-	pattern                     []int8
+	space                       [][caveWidth]*rock
+	pattern                     []int
 }
 
 func NewCave() *cave {
 	return &cave{
 		rockCount: 0,
-		jetCount:  0,
+		jetCycle:  0,
 		height:    0,
 		currRock:  nil,
-		space:     make([][7]*rock, 0),
-		pattern:   make([]int8, 0),
-	}
-}
-
-func (c *cave) tetrisMove(times int) {
-	for i := 0; i < times; i++ {
-		c.addRock()
-		cont := true
-		for cont {
-			c.applyJet()
-			cont = c.dropRock()
-		}
+		space:     make([][caveWidth]*rock, 0),
+		pattern:   make([]int, 0),
 	}
 }
 
@@ -42,16 +46,24 @@ func (c *cave) addRock() {
 	c.rockCount++
 
 	for len(c.space) <= c.height {
-		c.space = append(c.space, [7]*rock{nil, nil, nil, nil, nil, nil, nil})
+		var emptyRow [caveWidth]*rock
+		c.space = append(c.space, emptyRow)
 	}
 	for _, p := range c.currRock.pnts() {
 		c.space[p.y][p.x] = c.currRock
 	}
 }
 
+func (c *cave) dropOneRock() {
+	c.addRock()
+	for falling := true; falling; falling = c.rockFall() {
+		c.applyJet()
+	}
+}
+
 func (c *cave) applyJet() {
-	direction := c.pattern[c.jetCount%len(c.pattern)]
-	c.jetCount++
+	direction := c.pattern[c.jetCycle]
+	c.jetCycle = (c.jetCycle + 1) % len(c.pattern)
 
 	var add, rem []pnt
 	switch direction {
@@ -60,12 +72,10 @@ func (c *cave) applyJet() {
 	case 1:
 		add, rem = c.currRock.rightDelta()
 	}
-	// Collision check
 	for _, p := range add {
-		if p.x < 0 || p.x >= 7 {
+		if p.x < 0 || p.x >= caveWidth {
 			return
 		}
-
 		if c.space[p.y][p.x] != nil {
 			return
 		}
@@ -77,17 +87,16 @@ func (c *cave) applyJet() {
 	for _, p := range rem {
 		c.space[p.y][p.x] = nil
 	}
-	c.currRock.pos.x += int(direction)
+
+	c.currRock.pos.x += direction
 }
 
-func (c *cave) dropRock() bool {
+func (c *cave) rockFall() bool {
 	add, rem := c.currRock.downDelta()
-	// Collision check
 	for _, p := range add {
 		if p.y < 0 {
 			return false
 		}
-
 		if c.space[p.y][p.x] != nil {
 			return false
 		}
@@ -109,7 +118,6 @@ func (c *cave) dropRock() bool {
 	}
 
 	c.currRock.pos.y--
-
 	return true
 }
 
@@ -132,137 +140,120 @@ func (c cave) Print() {
 	fmt.Println(b.String())
 }
 
-type pnt struct {
-	x, y int
+func (c *cave) dropRocks(rocks int) {
+	for ; rocks > 0; rocks-- {
+		c.dropOneRock()
+	}
 }
 
 type rock struct {
-	pos      pnt
-	rockType int
-
-	// The following return (added, removed []pnt)
+	pos pnt
+	rt  rockType
 }
 
-func NewRock(rockType, caveHeight int) *rock {
+func NewRock(rt, caveHeight int) *rock {
 	r := new(rock)
-	r.rockType = rockType
-	r.setPos(caveHeight)
+	switch rt {
+	case 0:
+		r.rt = longRock{}
+	case 1:
+		r.rt = crossRock{}
+	case 2:
+		r.rt = lRock{}
+	case 3:
+		r.rt = tallRock{}
+	case 4:
+		r.rt = sqrRock{}
+	default:
+		panic("Invalid rock type")
+	}
+	r.pos.x, r.pos.y = 2, caveHeight+r.height()+2
 	return r
 }
 
-func (r *rock) setPos(caveHeight int) { r.pos.x, r.pos.y = 2, caveHeight+r.height()+2 }
+func (r *rock) height() int                { return r.rt.height() }
+func (r *rock) pnts() []pnt                { return r.rt.pnts(r.pos.x, r.pos.y) }
+func (r *rock) leftDelta() ([]pnt, []pnt)  { return r.rt.leftDelta(r.pos.x, r.pos.y) }
+func (r *rock) rightDelta() ([]pnt, []pnt) { return r.rt.rightDelta(r.pos.x, r.pos.y) }
+func (r *rock) downDelta() ([]pnt, []pnt)  { return r.rt.downDelta(r.pos.x, r.pos.y) }
 
-func (r *rock) height() int {
-	switch r.rockType {
-	case 0: // long
-		return 1
-	case 1: // cross
-		return 3
-	case 2: // reverse L
-		return 3
-	case 3: // tall
-		return 4
-	case 4: // square
-		return 2
-	default:
-		panic("Invalid rock type")
-	}
-	return -1
+type rockType interface {
+	height() int
+	pnts(int, int) []pnt
+	leftDelta(int, int) ([]pnt, []pnt)
+	rightDelta(int, int) ([]pnt, []pnt)
+	downDelta(int, int) ([]pnt, []pnt)
 }
+type longRock struct{}
+type crossRock struct{}
+type lRock struct{}
+type tallRock struct{}
+type sqrRock struct{}
 
-func (r *rock) pnts() []pnt {
-	x, y := r.pos.x, r.pos.y
-	switch r.rockType {
-	case 0: // long
-		return []pnt{pnt{x, y}, pnt{x + 1, y}, pnt{x + 2, y}, pnt{x + 3, y}}
-	case 1: // cross
-		return []pnt{
-			pnt{x + 1, y},
-			pnt{x, y - 1}, pnt{x + 1, y - 1}, pnt{x + 2, y - 1},
-			pnt{x + 1, y - 2},
-		}
-	case 2: // reverse L
-		return []pnt{
-			pnt{x + 2, y}, pnt{x + 2, y - 1}, pnt{x, y - 2}, pnt{x + 1, y - 2}, pnt{x + 2, y - 2},
-		}
-	case 3: // tall
-		return []pnt{pnt{x, y}, pnt{x, y - 1}, pnt{x, y - 2}, pnt{x, y - 3}}
-	case 4: // square
-		return []pnt{pnt{x, y}, pnt{x + 1, y}, pnt{x, y - 1}, pnt{x + 1, y - 1}}
-	default:
-		panic("Invalid rock type")
-	}
-	return nil
+func (lr longRock) height() int { return 1 }
+func (lr longRock) pnts(x, y int) []pnt {
+	return pkgPnts(x, y, x+1, y, x+2, y, x+3, y)
 }
-
-func (r *rock) leftDelta() ([]pnt, []pnt) {
-	var add, rem []pnt
-	x, y := r.pos.x, r.pos.y
-	switch r.rockType {
-	case 0: // long
-		add = []pnt{pnt{x - 1, y}}
-		rem = []pnt{pnt{x + 3, y}}
-	case 1: // cross
-		add = []pnt{pnt{x, y}, pnt{x - 1, y - 1}, pnt{x, y - 2}}
-		rem = []pnt{pnt{x + 1, y}, pnt{x + 2, y - 1}, pnt{x + 1, y - 2}}
-	case 2: // reverse L
-		add = []pnt{pnt{x + 1, y}, pnt{x + 1, y - 1}, pnt{x - 1, y - 2}}
-		rem = []pnt{pnt{x + 2, y}, pnt{x + 2, y - 1}, pnt{x + 2, y - 2}}
-	case 3: // tall
-		nx := x - 1
-		add = []pnt{pnt{nx, y}, pnt{nx, y - 1}, pnt{nx, y - 2}, pnt{nx, y - 3}}
-		rem = []pnt{pnt{x, y}, pnt{x, y - 1}, pnt{x, y - 2}, pnt{x, y - 3}}
-	case 4: // square
-		add = []pnt{pnt{x - 1, y}, pnt{x - 1, y - 1}}
-		rem = []pnt{pnt{x + 1, y}, pnt{x + 1, y - 1}}
-	}
-	return add, rem
+func (lr longRock) leftDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x-1, y), pkgPnts(x+3, y)
 }
-func (r *rock) rightDelta() ([]pnt, []pnt) {
-	var add, rem []pnt
-	x, y := r.pos.x, r.pos.y
-	switch r.rockType {
-	case 0: // long
-		add = []pnt{pnt{x + 4, y}}
-		rem = []pnt{pnt{x, y}}
-	case 1: // cross
-		add = []pnt{pnt{x + 2, y}, pnt{x + 3, y - 1}, pnt{x + 2, y - 2}}
-		rem = []pnt{pnt{x + 1, y}, pnt{x, y - 1}, pnt{x + 1, y - 2}}
-	case 2: // reverse L
-		add = []pnt{pnt{x + 3, y}, pnt{x + 3, y - 1}, pnt{x + 3, y - 2}}
-		rem = []pnt{pnt{x + 2, y}, pnt{x + 2, y - 1}, pnt{x, y - 2}}
-	case 3: // tall
-		nx := x + 1
-		add = []pnt{pnt{nx, y}, pnt{nx, y - 1}, pnt{nx, y - 2}, pnt{nx, y - 3}}
-		rem = []pnt{pnt{x, y}, pnt{x, y - 1}, pnt{x, y - 2}, pnt{x, y - 3}}
-	case 4: // square
-		add = []pnt{pnt{x + 2, y}, pnt{x + 2, y - 1}}
-		rem = []pnt{pnt{x, y}, pnt{x, y - 1}}
-	}
-	return add, rem
+func (lr longRock) rightDelta(x, y int) ([]pnt, []pnt) {
+	return []pnt{pnt{x + 4, y}}, []pnt{pnt{x, y}}
 }
-func (r *rock) downDelta() ([]pnt, []pnt) {
-	var add, rem []pnt
-	x, y := r.pos.x, r.pos.y
-	switch r.rockType {
-	case 0: // long
-		ny := y - 1
-		add = []pnt{pnt{x, ny}, pnt{x + 1, ny}, pnt{x + 2, ny}, pnt{x + 3, ny}}
-		rem = []pnt{pnt{x, y}, pnt{x + 1, y}, pnt{x + 2, y}, pnt{x + 3, y}}
-	case 1: // cross
-		add = []pnt{pnt{x, y - 2}, pnt{x + 1, y - 3}, pnt{x + 2, y - 2}}
-		rem = []pnt{pnt{x, y - 1}, pnt{x + 1, y}, pnt{x + 2, y - 1}}
-	case 2: // reverse L
-		add = []pnt{pnt{x, y - 3}, pnt{x + 1, y - 3}, pnt{x + 2, y - 3}}
-		rem = []pnt{pnt{x + 2, y}, pnt{x, y - 2}, pnt{x + 1, y - 2}}
-	case 3: // tall
-		add = []pnt{pnt{x, y - 4}}
-		rem = []pnt{pnt{x, y}}
-	case 4: // square
-		add = []pnt{pnt{x, y - 2}, pnt{x + 1, y - 2}}
-		rem = []pnt{pnt{x, y}, pnt{x + 1, y}}
-	}
-	return add, rem
+func (lr longRock) downDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y-1, x+1, y-1, x+2, y-1, x+3, y-1), pkgPnts(x, y, x+1, y, x+2, y, x+3, y)
+}
+func (cr crossRock) height() int { return 3 }
+func (cr crossRock) pnts(x, y int) []pnt {
+	return pkgPnts(x+1, y, x, y-1, x+1, y-1, x+2, y-1, x+1, y-2)
+}
+func (cr crossRock) leftDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y, x-1, y-1, x, y-2), pkgPnts(x+1, y, x+2, y-1, x+1, y-2)
+}
+func (cr crossRock) rightDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x+2, y, x+3, y-1, x+2, y-2), pkgPnts(x+1, y, x, y-1, x+1, y-2)
+}
+func (cr crossRock) downDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y-2, x+1, y-3, x+2, y-2), pkgPnts(x, y-1, x+1, y, x+2, y-1)
+}
+func (lr lRock) height() int { return 3 }
+func (lr lRock) pnts(x, y int) []pnt {
+	return pkgPnts(x+2, y, x+2, y-1, x, y-2, x+1, y-2, x+2, y-2)
+}
+func (lr lRock) leftDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x+1, y, x+1, y-1, x-1, y-2), pkgPnts(x+2, y, x+2, y-1, x+2, y-2)
+}
+func (lr lRock) rightDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x+3, y, x+3, y-1, x+3, y-2), pkgPnts(x+2, y, x+2, y-1, x, y-2)
+}
+func (lr lRock) downDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y-3, x+1, y-3, x+2, y-3), pkgPnts(x+2, y, x, y-2, x+1, y-2)
+}
+func (tr tallRock) height() int { return 4 }
+func (tr tallRock) pnts(x, y int) []pnt {
+	return pkgPnts(x, y, x, y-1, x, y-2, x, y-3)
+}
+func (tr tallRock) leftDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x-1, y, x-1, y-1, x-1, y-2, x-1, y-3), pkgPnts(x, y, x, y-1, x, y-2, x, y-3)
+}
+func (tr tallRock) rightDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x+1, y, x+1, y-1, x+1, y-2, x+1, y-3), pkgPnts(x, y, x, y-1, x, y-2, x, y-3)
+}
+func (tr tallRock) downDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y-4), pkgPnts(x, y)
+}
+func (sr sqrRock) height() int { return 2 }
+func (sr sqrRock) pnts(x, y int) []pnt {
+	return pkgPnts(x, y, x+1, y, x, y-1, x+1, y-1)
+}
+func (sr sqrRock) leftDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x-1, y, x-1, y-1), pkgPnts(x+1, y, x+1, y-1)
+}
+func (sr sqrRock) rightDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x+2, y, x+2, y-1), pkgPnts(x, y, x, y-1)
+}
+func (sr sqrRock) downDelta(x, y int) ([]pnt, []pnt) {
+	return pkgPnts(x, y-2, x+1, y-2), pkgPnts(x, y, x+1, y)
 }
 
 func readInput() []string {
@@ -278,7 +269,7 @@ func readInput() []string {
 }
 
 func parseInput(input []string) *cave {
-	pattern := make([]int8, len(input[0]))
+	pattern := make([]int, len(input[0]))
 	for i, r := range input[0] {
 		switch r {
 		case '<':
@@ -295,10 +286,12 @@ func parseInput(input []string) *cave {
 	return c
 }
 
+const goal = 2022
+
 func main() {
 	input := readInput()
 	c := parseInput(input)
 
-	c.tetrisMove(2022)
+	c.dropRocks(goal)
 	fmt.Println(c.height)
 }
