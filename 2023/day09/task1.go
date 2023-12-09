ca7d357e7b20ba20da763c4cd6b81fbf87b56e10
Kent Lui <kenthklui@gmail.com>
2023-12-09 00:45:38 -0500

    2023 day 9

---
+++ 2023/day09/task1.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kenthklui/adventofcode/util"
+)
+
+func binomial(row, col int) int {
+	n, r := row, col
+	if r+r > n {
+		r = n - r
+	}
+	product := 1
+	for i := 0; i < r; i++ {
+		product *= (n - i)
+	}
+	for i := r; i > 0; i-- {
+		product /= i
+	}
+	if col%2 == 0 {
+		return product
+	} else {
+		return -product
+	}
+}
+
+func history(intLine []int) int {
+	order := 1
+	for ; order < len(intLine); order++ {
+		allZero := true
+		for i := order; i < len(intLine); i++ {
+			sum := 0
+			for j := 0; j <= order; j++ {
+				sum += intLine[i-j] * binomial(order, j)
+			}
+			if sum != 0 {
+				allZero = false
+				break
+			}
+		}
+		if allZero {
+			break
+		}
+	}
+
+	nextValue := 0
+	for i := 1; i <= order; i++ {
+		nextValue -= intLine[len(intLine)-i] * binomial(order, i)
+	}
+
+	return nextValue
+}
+
+func main() {
+	input := util.StdinReadlines()
+	ints := util.ParseInts(input)
+
+	sum := 0
+	for _, intLine := range ints {
+		sum += history(intLine)
+	}
+	fmt.Println(sum)
+}
---
+++ 2023/day09/task2.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kenthklui/adventofcode/util"
+)
+
+func binomial(row, col int) int {
+	n, r := row, col
+	if r+r > n {
+		r = n - r
+	}
+	product := 1
+	for i := 0; i < r; i++ {
+		product *= (n - i)
+	}
+	for i := r; i > 0; i-- {
+		product /= i
+	}
+	if col%2 == 0 {
+		return product
+	} else {
+		return -product
+	}
+}
+
+func history(intLine []int) int {
+	order := 1
+	for ; order < len(intLine); order++ {
+		allZero := true
+		for i := order; i < len(intLine); i++ {
+			sum := 0
+			for j := 0; j <= order; j++ {
+				sum += intLine[i-j] * binomial(order, j)
+			}
+			if sum != 0 {
+				allZero = false
+				break
+			}
+		}
+		if allZero {
+			break
+		}
+	}
+
+	prevValue := 0
+	for i := 0; i < order; i++ {
+		prevValue -= intLine[order-1-i] * binomial(order, i)
+	}
+	prevValue *= binomial(order, order)
+	return prevValue
+}
+
+func main() {
+	input := util.StdinReadlines()
+	ints := util.ParseInts(input)
+
+	sum := 0
+	for _, intLine := range ints {
+		sum += history(intLine)
+	}
+	fmt.Println(sum)
+}
