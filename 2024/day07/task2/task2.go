b8392970400053a26500b8defdc052b4e4591864
Kent Lui <kenthklui@gmail.com>
2024-12-07 13:49:13 -0500

    2024 day 7

---
+++ 2024/day07/task2/task2.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/kenthklui/adventofcode/util"
+)
+
+func join(a, b int) int {
+	mult := 10
+	for b >= mult {
+		mult *= 10
+	}
+	return a*mult + b
+}
+
+type equation struct {
+	result int
+	inputs []int
+}
+
+func newEq(line string) equation {
+	ints := util.ParseLineInts(line)
+	return equation{ints[0], ints[1:]}
+}
+
+func (eq equation) solveFor(value int, inputs []int) bool {
+	if value > eq.result {
+		return false
+	}
+
+	if len(inputs) == 0 {
+		return value == eq.result
+	}
+
+	return eq.solveFor(value*inputs[0], inputs[1:]) || eq.solveFor(value+inputs[0], inputs[1:]) || eq.solveFor(join(value, inputs[0]), inputs[1:])
+}
+
+func (eq equation) isSolvable() bool {
+	return eq.solveFor(eq.inputs[0], eq.inputs[1:])
+}
+
+func solve(input []string) (output string) {
+	sum := 0
+	for _, line := range input {
+		eq := newEq(line)
+		if eq.isSolvable() {
+			sum += eq.result
+		}
+	}
+	return strconv.Itoa(sum)
+}
+
+func main() {
+	input := util.StdinReadlines()
+	solution := solve(input)
+	fmt.Println(solution)
+}
