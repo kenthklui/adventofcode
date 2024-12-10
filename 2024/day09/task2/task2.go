f65d6a1d225b6f2ca9ae4e121ef450578c8873e6
Kent Lui <kenthklui@gmail.com>
2024-12-10 14:48:34 -0800

    2024-09 Use min heap for task 2

--- 2024/day09/task2/task2.go
+++ 2024/day09/task2/task2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"strconv"
 
@@ -8,47 +9,71 @@ import (
 )
 
 type chunk struct {
-	id, pos, size int
+	pos, size int
 }
 
-func parse(line string) ([]chunk, []chunk) {
-	files, space := []chunk{}, []chunk{}
+type chunkHeap []chunk
+
+func (c chunkHeap) Len() int           { return len(c) }
+func (c chunkHeap) Less(i, j int) bool { return c[i].pos < c[j].pos }
+func (c chunkHeap) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+
+func (c *chunkHeap) Push(x interface{}) {
+	*c = append(*c, x.(chunk))
+}
+func (c *chunkHeap) Pop() interface{} {
+	old := *c
+	n := len(old)
+	x := old[n-1]
+	*c = old[:n-1]
+	return x
+}
+
+func parse(line string) ([]chunk, []chunkHeap) {
+	files, spaces := []chunk{}, make([]chunkHeap, 10)
 	totalSize := 0
 	for i, c := range line {
 		size := int(c - '0')
 		if i%2 == 0 {
-			files = append(files, chunk{i / 2, totalSize, size})
+			files = append(files, chunk{totalSize, size})
 		} else {
-			space = append(space, chunk{0, totalSize, size})
+			spaces[size] = append(spaces[size], chunk{totalSize, size})
 		}
 		totalSize += size
 	}
 
-	return files, space
+	for i := range spaces {
+		heap.Init(&spaces[i])
+	}
+	return files, spaces
 }
 
 func checksum(files []chunk) int {
 	checksum := 0
-	for _, file := range files {
+	for id, file := range files {
 		// sum(n, n+1, ..., n+k) = sum(1, ..., n+k) - sum(1, ..., n-1)
 		fileEnd := file.pos + file.size - 1
-		checksum += (fileEnd*(fileEnd+1) - (file.pos-1)*file.pos) / 2 * file.id
+		checksum += (fileEnd*(fileEnd+1) - (file.pos-1)*file.pos) / 2 * id
 	}
 	return checksum
 }
 
 func solve(input []string) (output string) {
-	files, space := parse(input[0])
+	files, spaces := parse(input[0])
 	for i := len(files) - 1; i > 0; i-- {
-		for j := range space {
-			if files[i].pos < space[j].pos {
-				break
+		earliest := files[i]
+		for j := files[i].size; j < len(spaces); j++ {
+			if len(spaces[j]) > 0 && earliest.pos > spaces[j][0].pos {
+				earliest = spaces[j][0]
 			}
-			if files[i].size <= space[j].size {
-				files[i].pos = space[j].pos
-				space[j].size -= files[i].size
-				space[j].pos += files[i].size
-			}
+		}
+		if earliest.pos < files[i].pos {
+			files[i].pos = earliest.pos
+
+			heap.Pop(&spaces[earliest.size])
+			earliest.size -= files[i].size
+			earliest.pos += files[i].size
+			heap.Push(&spaces[earliest.size], earliest)
 		}
 	}
 	return strconv.Itoa(checksum(files))
